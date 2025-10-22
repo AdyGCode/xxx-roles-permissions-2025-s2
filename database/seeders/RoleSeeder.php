ca25a461d66f87cb57830dd8ca84df7a634a8da3
Ady G (Work) <Adrian.Gould@nmtafe.wa.edu.au>
2025-10-22 17:15:49 +0800

    chore: start of code

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */11
+    public function run(): void
+    {
+        $seedRoles = [
+            ['name' => 'super-user'],
+            ['name' => 'admin'],
+            ['name' => 'staff'],
+            ['name' => 'client'],
+        ];
+
+        foreach ($seedRoles as $seedRole) {
+            Role::create($seedRole);
+        }
+    }
+}
