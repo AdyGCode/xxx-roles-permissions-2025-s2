c08c5eaeba849f833b0538f816c6920e3549aeee
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2025-11-13 09:08:03 +0800

    wip:  Roles and Perms part 2 and 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,9 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Spatie\Permission\Middleware\PermissionMiddleware;
-use Spatie\Permission\Middleware\RoleMiddleware;
-use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
